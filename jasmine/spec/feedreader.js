5acda4d93f19c19c59d8c84793022abb73414c7e
Ben Thomas <bthomas2622@gmail.com>
2016-01-02 11:47:30 -0500

    added jasmine tests to check that urls in feed exist and have names defined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('should have non-empty url defined for each feed', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined(); //check url defined
+                expect(feed.url.length).not.toBe(0); //check url empty
+            });
+         });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('should have urls with names defined and existent', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined(); //check name defined
+                expect(feed.name.length).not.toBe(0); //check name empty
+            });
+         });
     });
 
 
